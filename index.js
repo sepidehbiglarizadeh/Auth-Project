9e4add5cedbe0e25fcc1449c5888f21447b8694d
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-15 16:17:58 +0330

    new structure - the first part

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const debug = require("debug")("app:main");
+const config = require("config");
+const router = require("./src/routes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose
+  .connect(config.get("db.address"))
+  .then(() => debug("Connected to mongodb"))
+  .catch(() => debug("Could not connect"));
+
+app.use("/api", router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
