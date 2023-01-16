229ad95b4be6363aa1a0228cfee2a831a0da80cd
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-16 15:42:49 +0330

    login user

--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -1,6 +1,8 @@
 const Controller = require("../controller");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 module.exports = new (class extends Controller {
   register = async (req, res) => {
@@ -28,6 +30,32 @@ module.exports = new (class extends Controller {
   };
 
   login = async (req, res) => {
-    res.send("Login");
+    //? find user
+    const user = await this.User.findOne({ email: req.body.email });
+    if (!user) {
+      return this.response({
+        res,
+        code: 400,
+        message: "incvalid email or password",
+      });
+    }
+
+    //? comparing the password with the existing password
+    const isValid = await bcrypt.compare(req.body.password, user.password);
+    if (!isValid) {
+      return this.response({
+        res,
+        code: 400,
+        message: "invalid email or password",
+      });
+    }
+
+    //? create token
+    const token = jwt.sign({ _id: user.id }, config.get("jwt.key"));
+    this.response({
+      res,
+      message: "succesfully logged in",
+      data: { token },
+    });
   };
 })();
